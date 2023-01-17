8623fe6e4d7f7c8dd6138c27cffbbf5280ae4228
Aman <undefineaman@gmail.com>
2023-01-17 18:08:31 +0600

    still

---
+++ Array/myscript.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(function() {
+        // Owl Carousel
+        var owl = $(".owl-carousel");
+        owl.owlCarousel({
+        items: 3,
+        margin: 10,
+        autoplay:true,
+        loop: true,
+        nav: true
+        });
+    });
+});
