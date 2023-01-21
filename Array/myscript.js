f81098887e5a9fbe941c7d4addbdde706a50f701
Aman <undefineaman@gmail.com>
2023-01-21 20:25:29 +0600

    holly

--- Array/myscript.js
+++ Array/myscript.js
@@ -3,11 +3,28 @@ $(document).ready(function(){
         // Owl Carousel
         var owl = $(".owl-carousel");
         owl.owlCarousel({
-        items: 3,
-        margin: 10,
-        autoplay:true,
-        loop: true,
-        nav: true
+            items: 3,
+            margin: 20,
+            autoplay:true,
+            autoplayTimeOUt:2000,
+            autoplayHOverPause:true,
+            loop: true,
+            nav: true,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false
+                },
+                600: {
+                    items: 2,
+                    nav: false
+                },
+                1000: {
+                    items: 3,
+                    nav: false
+                },
+    
+            }
         });
     });
 });
