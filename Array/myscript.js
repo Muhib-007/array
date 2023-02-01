f8dee6f6108b4bc365fae7686b95e868adc6ca85
Aman <undefineaman@gmail.com>
2023-02-01 20:33:17 +0600

    ready for any uses

--- Array/myscript.js
+++ Array/myscript.js
@@ -28,5 +28,5 @@ $(document).ready(function(){
         });
     });
 });
-onclick = 
-            "alert('Welcome to GeeksforGeeks')"
+// onclick = 
+//             "alert('Welcome to GeeksforGeeks')"
